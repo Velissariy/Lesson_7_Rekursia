9dd526fc0313cc451f8e330506929a9ee0cd1d11
Juno <polya.alexei@yandex.ru>
2023-03-06 14:51:29 +0300

    added tasks 6-8

--- Tasks_001/Program.cs
+++ Tasks_001/Program.cs
@@ -108,3 +108,84 @@
     
 //   }
 // }
+
+// Task_5.1
+// int n = 1;
+// void FindWords(string alphabet, char[] word, int length = 0)
+// {
+//   if (length == word.Length)
+//   {
+//     Console.WriteLine($"{n++} {new String(word)}"); return;
+//   }
+//   for (int i = 0; i < alphabet.Length; i++)
+//   {
+//     word[length] = alphabet[i];
+//     FindWords(alphabet, word, length+1);
+//   }
+// }
+
+// FindWords("аисв", new char[2]);
+
+// Task_6
+//обход директорий
+
+// string path = "D:/GeekBrains/7_С#/Projects_C#/C#_test";
+// DirectoryInfo di = new DirectoryInfo(path);
+// System.Console.WriteLine(di.CreationTime);
+// FileInfo[] fi = di.GetFiles();
+
+// for (int i = 0; i < fi.Length; i++)
+// {
+//   System.Console.WriteLine(fi[i].Name);
+// }
+
+// Task_6.1
+// void CatalogInfo(string path, string indent ="")
+// {
+//   DirectoryInfo catalog = new DirectoryInfo(path);
+//   DirectoryInfo[] catalogs = catalog.GetDirectories();
+//   for (int i = 0; i < catalogs.Length; i++)
+//   {
+//     Console.WriteLine($"{indent}{catalogs[i].Name}");
+//     CatalogInfo(catalogs[i].FullName, indent +" ");
+//   }
+
+//   FileInfo[] files = catalog.GetFiles();
+//   for (int i = 0; i < files.Length; i++)
+//   {
+//     Console.WriteLine($"{indent}{files[i].Name}");
+//   }
+// }
+// string path = "D:/GeekBrains/7_С#/Projects_C#/C#_test";
+// CatalogInfo(path);
+
+
+//Tasks_7;
+
+// void Towers(string with ="1", string on = "3", string some = "2", int count = 3)
+// {
+//   if (count>1) Towers(with, some, on, count -1);
+//   Console.WriteLine($"{with} >> {on}");
+//   if (count>1) Towers(some,on, with, count -1);
+// }
+
+// Towers();
+
+//Tasks_8;
+//Обход разных структур
+// ((4-2)*(1+3))/10
+string emp = String.Empty;
+string[] tree = { emp, "/", "*", "10", "-", "+", emp, emp, "4", "2", "1", "3"};
+//                 0    1    2     3    4    5    6    7    8    9    10   11
+void InOrderTraversal(int pos = 1)
+{
+  if (pos < tree.Length)
+  {
+    int left = 2 * pos;
+    int right = 2 * pos + 1;
+    if (left<tree.Length &&!String.IsNullOrEmpty(tree[left])) InOrderTraversal(left);
+    Console.WriteLine(tree[pos]);
+    if (right<tree.Length &&!String.IsNullOrEmpty(tree[right])) InOrderTraversal(right);
+  }
+}
+InOrderTraversal();
